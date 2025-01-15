e751d55a1c3c16acb1d2531163b1a129e5cda3cf
Yaseen Saad Eldin <yaseensaadeldinmohamed@gmail.com>
2025-01-15 17:35:34 +0200

    gjojojqocqwjco

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
-app.use(express.static(path.join(__dirname, './public')));
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.post("/submit", async (req, res) => {
     try {
